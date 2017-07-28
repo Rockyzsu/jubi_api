969d8691fd97b38d6448dd3503272c6924322237
Rocky Chen <jinweizsu@gmail.com>
2017-07-28 22:48:34 +0800

    基本api可用

--- basic_usage.py
+++ basic_usage.py
@@ -56,16 +56,51 @@ class api_demo():
                 hv = '0' + hv
             lst.append(hv)
         return reduce(lambda x, y: x + y, lst)
-    '''
-    参数
-    key - API key
-    signature - signature
-    nonce - nonce
-    since - unix timestamp(utc timezone) default == 0, i.e. 返回所有
-    coin - 币种简称,例如btc、ltc、xas
-    type - 挂单类型[open:正在挂单, all:所有挂单]
-    '''
+
+    def account_balance(self):
+        url=self.host+'/api/v1/balance/'
+        nonce=self.get_nonce_time()
+        parameters={'key':self.public_key,'nonce':str(nonce),'signature':''}
+        post_data=''
+        for k,v in parameters.items():
+            post_data=post_data+k+'='+v+'&'
+
+        post_data=post_data.replace('&signature=&','')
+        #print post_data
+        signature=signature =hmac.new(self.md5,post_data,digestmod=hashlib.sha256).digest()
+        sig=self.toHex(signature)
+        parameters['signature']=sig
+        r=requests.post(url,data=parameters)
+        s=r.json()
+        #print s
+        for k,v in s.items():
+
+            #print k,v
+            if v !=0:
+                print k,v
+        return s
+
     def Trade_list(self,coin):
+        '''
+        Trade_list（挂单查询）
+        您指定时间后的挂单，可以根据类型查询，比如查看正在挂单和全部挂单
+        Path：/api/v1/trade_list/
+        Request类型：POST
+        参数
+        key - API key
+        signature - signature
+        nonce - nonce
+        since - unix timestamp(utc timezone) default == 0, i.e. 返回所有
+        coin - 币种简称,例如btc、ltc、xas
+        type - 挂单类型[open:正在挂单, all:所有挂单]
+        返回JSON dictionary
+        id - 挂单ID
+        datetime - date and time
+        type - "buy" or "sell"
+        price - price
+        amount_original - 下单时数量
+        amount_outstanding - 当前剩余数量
+        '''
         url=self.host+'/api/v1/trade_list/'
         nonce=self.get_nonce_time()
         types='all'
@@ -98,21 +133,27 @@ class api_demo():
             #print i
             pass
         return s
-    '''
-    Trade_add（下单）
-    Path：/api/v1/trade_add/
-    Request类型：POST
-    参数
-    key - API key
-    signature - signature
-    nonce - nonce
-    amount - 购买数量
-    price - 购买价格
-    type - 买单或者卖单
-    coin - 币种简称,例如btc、ltc、xas
-    返回JSON dictionary
-    '''
+
     def Trade_add(self,coin,amount,price):
+
+        '''
+        Trade_add（下单）
+        Path：/api/v1/trade_add/
+        Request类型：POST
+        参数
+        key - API key
+        signature - signature
+        nonce - nonce
+        amount - 购买数量
+        price - 购买价格
+        type - 买单或者卖单
+        coin - 币种简称,例如btc、ltc、xas
+        返回JSON dictionary
+        id - 挂单ID
+        result - true(成功), false(失败)
+        返回结果示例：
+        {"result":true, "id":"11"}
+        '''
         url=self.host+'/api/v1/trade_add/'
         nonce=self.get_nonce_time()
         types='buy'
@@ -141,19 +182,24 @@ class api_demo():
         s=r.json()
         print s
 
-    '''
-    Trade_cancel（取消订单）
-    Path：/api/v1/trade_cancel/
-    Request类型：POST
-    参数
-    key - API key
-    signature - signature
-    nonce - nonce
-    id - 挂单ID
-    coin - 币种简称,例如btc、ltc、xas
-    返回JSON dictionary
-    '''
+
     def Trade_cancel(self,coin,id_num):
+        '''
+        Trade_cancel（取消订单）
+        Path：/api/v1/trade_cancel/
+        Request类型：POST
+        参数
+        key - API key
+        signature - signature
+        nonce - nonce
+        id - 挂单ID
+        coin - 币种简称,例如btc、ltc、xas
+        返回JSON dictionary
+        result - true(成功), false(失败)
+        id - 订单ID
+        返回结果示例：
+        {"result":true, "id":"11"}
+        '''
         url=self.host+'/api/v1/trade_cancel/'
         nonce=self.get_nonce_time()
 
@@ -172,30 +218,31 @@ class api_demo():
         print s
 
 
-    '''
-    Trade_view（查询订单信息）
-    Path：/api/v1/trade_view/
-    Request类型：POST
-    参数
-    key - API key
-    signature - signature
-    nonce - nonce
-    id - 挂单ID
-    coin - 币种简称,例如btc、ltc、xas
-    '''
 
-    '''
-    返回JSON dictionary
-    id - 挂单ID
-    datetime - 挂单时间（格式：YYYY-mm-dd HH:ii:ss）
-    type - "buy" or "sell"
-    price - 挂单价
-    amount_original - 下单时数量
-    amount_outstanding - 当前剩余数量
-    status - 状态：new(新挂单), open(开放交易), cancelled(撤消), closed(完全成交)
-    avg_price - 成交均价
-    '''
     def Trade_view(self,coin,id_num):
+        '''
+        Trade_view（查询订单信息）
+        Path：/api/v1/trade_view/
+        Request类型：POST
+        参数
+            key - API key
+            signature - signature
+            nonce - nonce
+            id - 挂单ID
+            coin - 币种简称,例如btc、ltc、xas
+
+
+
+            返回JSON dictionary
+            id - 挂单ID
+            datetime - 挂单时间（格式：YYYY-mm-dd HH:ii:ss）
+            type - "buy" or "sell"
+            price - 挂单价
+            amount_original - 下单时数量
+            amount_outstanding - 当前剩余数量
+            status - 状态：new(新挂单), open(开放交易), cancelled(撤消), closed(完全成交)
+            avg_price - 成交均价
+        '''
         url=self.host+'/api/v1/trade_view/'
         nonce=self.get_nonce_time()
 
@@ -213,6 +260,8 @@ class api_demo():
         r=requests.post(url=url,data=parameters)
         s=r.json()
         print s
+
+
     def Data_transform(self):
         data=self.Trade_list('zet')
         df=pd.DataFrame(data)
@@ -231,7 +280,8 @@ def main():
     #obj.Trade_cancel('zet','862979')
     #obj.Data_transform()
     #obj.Trade_view('zet','864111')
-    obj.Data_transform()
+    #obj.Data_transform()
+    obj.account_balance()
 
 if __name__=='__main__':
     main()
