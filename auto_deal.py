8253634049db30b25825e7a9ea0e46725e5575fd
Rocky Chen <jinweizsu@gmail.com>
2017-07-29 11:59:18 +0800

    只对有效的委托进行撤销

--- auto_deal.py
+++ auto_deal.py
@@ -4,63 +4,74 @@
 import random,json
 import hashlib
 import hmac,time
-import smtplib
-from email.mime.text import MIMEText
 from email import Utils
 import threading
 import requests,datetime,itchat
 import urllib,urllib2
 from toolkit import Toolkit
+import Queue
+from basic_usage import api_demo
+import pandas as pd
+import numpy as np
 
-class Jubi_access():
-    def __init__(self):
-        cfg = Toolkit.getUserData('data.cfg')
-        self.public_key = cfg['public_key']
-        self.private_key = cfg['private_key']
-        self.host='https://www.jubi.com'
+obj=api_demo()
+q=Queue.Queue()
+data_dict={}
+def price_strategy(coin,price,amount):
+    '''
+    当价格满足要求的时候就下单
+    :return:
+    '''
+
+    '''
+    t_id=obj.Trade_add(coin,price,amount)
+    print t_id
+    data_dict={t_id:[coin,price,amount]}
+    '''
+
+    s=obj.Trade_list('zet')
+    '''
+    for i in s:
+        #print k,v
+        print i
+    '''
 
 
-        self.coin_name={'zet':u'泽塔币',
-                        'doge':u'狗狗币',
-                        'eac':u'地球币',
-                        'dnc':u'暗网币',
-                        'rio':u'里约币',
-                        'blk':u'黑币',
-                        'ifc':u'无限币',
-                        'met':u'美通币',
-                        'gooc':u'谷壳币',
-                        'jbc':u'聚宝币',
-                        'pgc':u'乐通币',
-                        'lsk':u'LISK',
-                        'tfc':u'传送币',
-                        'xpm':u'质数币',
-                        'nxt':u'未来币',
-                        'ppc':u'点点币',
-                        'ktc':u'肯特币',
-                        'mtc':u'猴宝币',
-                        'skt':u'鲨之信',
-                        'btc':u'比特币',
-                        'peb':u'普银币',
-                        'ltc':u'莱特币',
-                        'xsgs':u'雪山古树',
-                        'eth':u'以太坊',
-                        'vtc':u'绿币',
-                        'bts':u'比特股',
-                        'hlb':u'活力币',
-                        'zcc':u'招财币',
-                        'etc':u'以太经典',
-                        'qec':u'企鹅币',
-                        'fz':u'冰河币',
-                        'plc':u'保罗币',
-                        'max':u'最大币',
-                        'ytc':u'一号币',
-                        'xrp':u'瑞波币',
-                        'lkc':u'幸运币',
-                        'wdc':u'世界币',
-                        'vrc':u'维理币',
-                        'rss':u'红贝壳',
-                        'ans':u'小蚁股',
-                        'xas':u'阿希比',
-                        'game':u'游戏点',
-                        'mryc':u'美人鱼币',
-                            }
+    #df=pd.DataFrame(s,index_col='datetime')
+    #df=pd.DataFrame(s,index=[u'datetime'])
+    df=pd.DataFrame(s)
+    #print df
+
+    #df['datetime']=df['datetime'].map(lambda x:datetime.datetime.strptime(x,'%Y-%m-%d %H:%M:%S'))
+    #上面这两种方法都可以
+    df['datetime'] = pd.to_datetime(df['datetime'])
+
+    df.set_index('datetime',inplace=True,drop=True)
+
+    #print df.dtypes
+    #print df.info()
+    #print df
+    df['status']=df['id'].map(lambda x:obj.Trade_view('zet',x)['status'])
+
+    print df
+    new_df = df[df['status']=='open']
+
+    #df.to_csv('')
+    print new_df
+
+
+
+
+def main():
+
+    price_strategy('zet',0.0112,1000)
+    #price_strategy('zet',0.011,1000)
+
+    while q.empty() is False:
+        print q.get()
+
+
+
+if __name__=='__main__':
+    main()
+
--- basic_usage.py
+++ basic_usage.py
@@ -18,23 +18,23 @@ import pandas as pd
 class api_demo():
     def __init__(self):
 
-        self.host='http://www.jubi.com'
+        self.host = 'http://www.jubi.com'
 
         with open('coin_list.cfg') as f:
-            c_content=f.read()
+            c_content = f.read()
 
-        d_content=json.loads(c_content)
+        self.d_content = json.loads(c_content)
 
-        #x=zip(* d_content)
+        # x=zip(* d_content)
         #print x
         #为什么得不到想要的结果?
 
-        self.coin_list=d_content.keys()
-        self.coin_name=d_content.values()
+        self.coin_list = self.d_content.keys()
+        self.coin_name = self.d_content.values()
 
-        self.private_key=Toolkit.getUserData('data.cfg')['private_key']
-        self.public_key=Toolkit.getUserData('data.cfg')['public_key']
-        self.md5=self.getHash(self.private_key)
+        self.private_key = Toolkit.getUserData('data.cfg')['private_key']
+        self.public_key = Toolkit.getUserData('data.cfg')['public_key']
+        self.md5 = self.getHash(self.private_key)
 
 
     def getHash(self, s):
@@ -44,11 +44,11 @@ class api_demo():
 
     def get_nonce_time(self):
         lens = 12
-        curr_stamp = time.time()*100
-        nonece=long(curr_stamp)
+        curr_stamp = time.time() * 100
+        nonece = long(curr_stamp)
         return nonece
 
-    def toHex(self,str):
+    def toHex(self, str):
         lst = []
         for ch in str:
             hv = hex(ord(ch)).replace('0x', '')
@@ -58,29 +58,52 @@ class api_demo():
         return reduce(lambda x, y: x + y, lst)
 
     def account_balance(self):
-        url=self.host+'/api/v1/balance/'
-        nonce=self.get_nonce_time()
-        parameters={'key':self.public_key,'nonce':str(nonce),'signature':''}
-        post_data=''
-        for k,v in parameters.items():
-            post_data=post_data+k+'='+v+'&'
+        '''
+        列举您的帐户信息
+        Path：/api/v1/balance/
+        Request类型：POST
+        参数
+        key - API key
+        signature - signature
+        nonce - nonce
+        返回JSON dictionary
+        asset - 折合人民币总余额
+        cny_balance - 人民币总余额
+        ltc_balance - 比特币总余额
+        cny_lock - 人民币冻结余额
+        ltc_lock - 比特币冻结余额
+        ............
+        nameauth - 实名认证状态 0 未实名 1 等待确认 2 已经认证成功
+        moflag - 手机绑定状态 0 未绑定 1 绑定
+        返回结果示例：
+        {"uid":8,"nameauth":0,"moflag":0,"doge_balance":4234234,"doge_lock":0,"ltc_balance":32429.6,"ltc_lock":2.4,"xpm_balance":0,"xpm_lock":0,"cny_balance":2344581.519,"cny_lock":868862.481}
 
-        post_data=post_data.replace('&signature=&','')
-        #print post_data
-        signature=signature =hmac.new(self.md5,post_data,digestmod=hashlib.sha256).digest()
-        sig=self.toHex(signature)
-        parameters['signature']=sig
-        r=requests.post(url,data=parameters)
-        s=r.json()
+        :return:
+        '''
+
+        url = self.host + '/api/v1/balance/'
+        nonce = self.get_nonce_time()
+        parameters = {'key': self.public_key, 'nonce': str(nonce), 'signature': ''}
+        post_data = ''
+        for k, v in parameters.items():
+            post_data = post_data + k + '=' + v + '&'
+
+        post_data = post_data.replace('&signature=&', '')
+        # print post_data
+        signature = signature = hmac.new(self.md5, post_data, digestmod=hashlib.sha256).digest()
+        sig = self.toHex(signature)
+        parameters['signature'] = sig
+        r = requests.post(url, data=parameters)
+        s = r.json()
         #print s
-        for k,v in s.items():
+        for k, v in s.items():
 
             #print k,v
-            if v !=0:
-                print k,v
+            if v != 0:
+                print k, v
         return s
 
-    def Trade_list(self,coin):
+    def Trade_list(self, coin):
         '''
         Trade_list（挂单查询）
         您指定时间后的挂单，可以根据类型查询，比如查看正在挂单和全部挂单
@@ -93,6 +116,7 @@ class api_demo():
         since - unix timestamp(utc timezone) default == 0, i.e. 返回所有
         coin - 币种简称,例如btc、ltc、xas
         type - 挂单类型[open:正在挂单, all:所有挂单]
+
         返回JSON dictionary
         id - 挂单ID
         datetime - date and time
@@ -101,40 +125,41 @@ class api_demo():
         amount_original - 下单时数量
         amount_outstanding - 当前剩余数量
         '''
-        url=self.host+'/api/v1/trade_list/'
-        nonce=self.get_nonce_time()
-        types='all'
-        #since=0
-        parameters={'key':self.public_key,'nonce':str(nonce),'type':types,'coin':coin,'signature':''}
+        url = self.host + '/api/v1/trade_list/'
+        nonce = self.get_nonce_time()
+        types = 'all'
+        # since=0
+        parameters = {'key': self.public_key, 'nonce': str(nonce), 'type': types, 'coin': coin, 'signature': ''}
         #print parameters
-        post_data=''
-        for k,v in parameters.items():
-            if not isinstance(v,str) :
-            #if type(v) is not types.StringType:
-                v=str(v)
-            post_data=post_data+k
-            post_data=post_data+'='+v+'&'
+        post_data = ''
+        for k, v in parameters.items():
+            if not isinstance(v, str):
+                #if type(v) is not types.StringType:
+                v = str(v)
+            post_data = post_data + k
+            post_data = post_data + '=' + v + '&'
 
         #print 'post-data:\n',post_data
-        post_data=post_data[:-1]
-        post_data=post_data.replace('&signature=','')
+        post_data = post_data[:-1]
+        post_data = post_data.replace('&signature=', '')
         #print post_data
 
-        signature =hmac.new(self.md5,post_data,digestmod=hashlib.sha256).digest()
-        sig=self.toHex(signature)
-        parameters['signature']=sig
+        signature = hmac.new(self.md5, post_data, digestmod=hashlib.sha256).digest()
+        sig = self.toHex(signature)
+        parameters['signature'] = sig
         #print parameters
-        r=requests.post(url=url,data=parameters)
-        s=r.json()
+        r = requests.post(url=url, data=parameters)
+        s = r.json()
         #print s
         #print type(s)
-
+        '''
         for i in s:
             #print i
             pass
+        '''
         return s
 
-    def Trade_add(self,coin,amount,price):
+    def Trade_add(self, coin,  price,amount):
 
         '''
         Trade_add（下单）
@@ -154,36 +179,41 @@ class api_demo():
         返回结果示例：
         {"result":true, "id":"11"}
         '''
-        url=self.host+'/api/v1/trade_add/'
-        nonce=self.get_nonce_time()
-        types='buy'
-        #amount=100
+        url = self.host + '/api/v1/trade_add/'
+        nonce = self.get_nonce_time()
+        types = 'buy'
+        # amount=100
         #price=0.1
-        parameters={'key':self.public_key,'nonce':str(nonce),'type':types,'coin':coin,'signature':'','amount':amount,'price':price}
-        print parameters
-        post_data=''
-        for k,v in parameters.items():
-            if not isinstance(v,str) :
-            #if type(v) is not types.StringType:
-                v=str(v)
-            post_data=post_data+k
-            post_data=post_data+'='+v+'&'
+        parameters = {'key': self.public_key, 'nonce': str(nonce), 'type': types, 'coin': coin, 'signature': '',
+                      'amount': amount, 'price': price}
+        #print parameters
+        post_data = ''
+        for k, v in parameters.items():
+            if not isinstance(v, str):
+                #if type(v) is not types.StringType:
+                v = str(v)
+            post_data = post_data + k
+            post_data = post_data + '=' + v + '&'
 
-        print 'post-data:\n',post_data
-        post_data=post_data[:-1]
-        print post_data
-        post_data=post_data.replace('signature=&','')
-        print post_data
-        signature =hmac.new(self.md5,post_data,digestmod=hashlib.sha256).digest()
-        sig=self.toHex(signature)
-        parameters['signature']=sig
-        print parameters
-        r=requests.post(url=url,data=parameters)
-        s=r.json()
+        #print 'post-data:\n', post_data
+        post_data = post_data[:-1]
+        #print post_data
+        post_data = post_data.replace('signature=&', '')
+        #print post_data
+        signature = hmac.new(self.md5, post_data, digestmod=hashlib.sha256).digest()
+        sig = self.toHex(signature)
+        parameters['signature'] = sig
+        #print parameters
+        r = requests.post(url=url, data=parameters)
+        s = r.json()
         print s
+        if s['result'] is True:
+            return s['id']
+        #return s
+        return False
 
 
-    def Trade_cancel(self,coin,id_num):
+    def Trade_cancel(self, coin, id_num):
         '''
         Trade_cancel（取消订单）
         Path：/api/v1/trade_cancel/
@@ -200,26 +230,26 @@ class api_demo():
         返回结果示例：
         {"result":true, "id":"11"}
         '''
-        url=self.host+'/api/v1/trade_cancel/'
-        nonce=self.get_nonce_time()
+        url = self.host + '/api/v1/trade_cancel/'
+        nonce = self.get_nonce_time()
 
-        parameters={'nonce':str(nonce),'coin':coin,'id':id_num,'key':self.public_key,'signature':''}
-        post_data=''
-        for k,v in parameters.items():
-            post_data=post_data+k+'='+v+'&'
-        post_data=post_data[:-1]
-        print post_data
-        post_data=post_data.replace('&signature=','')
-        signature =hmac.new(self.md5,post_data,digestmod=hashlib.sha256).digest()
-        sig=self.toHex(signature)
-        parameters['signature']=sig
-        r=requests.post(url=url,data=parameters)
-        s=r.json()
+        parameters = {'nonce': str(nonce), 'coin': coin, 'id': id_num, 'key': self.public_key, 'signature': ''}
+        post_data = ''
+        for k, v in parameters.items():
+            post_data = post_data + k + '=' + v + '&'
+        post_data = post_data[:-1]
+        #print post_data
+        post_data = post_data.replace('&signature=', '')
+        signature = hmac.new(self.md5, post_data, digestmod=hashlib.sha256).digest()
+        sig = self.toHex(signature)
+        parameters['signature'] = sig
+        r = requests.post(url=url, data=parameters)
+        s = r.json()
         print s
+        return s['result']
 
 
-
-    def Trade_view(self,coin,id_num):
+    def Trade_view(self, coin, id_num):
         '''
         Trade_view（查询订单信息）
         Path：/api/v1/trade_view/
@@ -243,45 +273,79 @@ class api_demo():
             status - 状态：new(新挂单), open(开放交易), cancelled(撤消), closed(完全成交)
             avg_price - 成交均价
         '''
-        url=self.host+'/api/v1/trade_view/'
-        nonce=self.get_nonce_time()
+        url = self.host + '/api/v1/trade_view/'
+        nonce = self.get_nonce_time()
 
-        parameters={'nonce':str(nonce),'coin':coin,'id':id_num,'key':self.public_key,'signature':''}
-        post_data=''
-        for k,v in parameters.items():
-            post_data=post_data+k+'='+v+'&'
-        post_data=post_data[:-1]
-        #print post_data
-        post_data=post_data.replace('&signature=','')
-        signature =hmac.new(self.md5,post_data,digestmod=hashlib.sha256).digest()
-        sig=self.toHex(signature)
-        parameters['signature']=sig
+        parameters = {'nonce': str(nonce), 'coin': coin, 'id': id_num, 'key': self.public_key, 'signature': ''}
+        post_data = ''
+        for k, v in parameters.items():
+            post_data = post_data + k + '=' + v + '&'
+        post_data = post_data[:-1]
+        # print post_data
+        post_data = post_data.replace('&signature=', '')
+        signature = hmac.new(self.md5, post_data, digestmod=hashlib.sha256).digest()
+        sig = self.toHex(signature)
+        parameters['signature'] = sig
 
-        r=requests.post(url=url,data=parameters)
-        s=r.json()
+        r = requests.post(url=url, data=parameters)
+        s = r.json()
         print s
+        return s
 
 
-    def Data_transform(self):
-        data=self.Trade_list('zet')
-        df=pd.DataFrame(data)
 
-        #df['datetime']=df['datetime'].applymap(lambda x:)
-        df['datetime']=pd.to_datetime(df['datetime'])
+    def AllTicker(self):
+        url=self.host+'/api/v1/allticker/'
+        r=requests.get(url)
+        s=r.json()
+        #print s
+        '''
+        for k,v in s.items():
+            print self.d_content[k]
+            for vk,vv in v.items():
+                print vk,vv
+            print '*'*15
+        '''
+        return s
+    def Cancel_All(self,coin):
+        df=self.Data_transform(coin)
+        res=df['id'].map(lambda x:self.Trade_cancel(coin,x))
+        print res
+        #self.Trade_cancel(coin,)
+
+    def Data_transform(self,coin):
+        data = self.Trade_list(coin)
+        df = pd.DataFrame(data)
+
+        # df['datetime']=df['datetime'].applymap(lambda x:)
+        df['datetime'] = pd.to_datetime(df['datetime'])
         #print df
-        #rint df.info()
-        #print df.dtypes
+        df.set_index('datetime',inplace=True,drop=True)
+        print df
+        print df.info()
+        print df.dtypes
+        '''
         for i in df['id'].values:
-            self.Trade_view('zet',i)
+            self.Trade_view('zet', i)
+        '''
+        df['status']=df['id'].map(lambda x:self.Trade_view(coin,x)['status'])
+        df=df[df[u'status']==u'open']
+        self.current_order=df
+        return self.current_order
 
 def main():
-    obj=api_demo()
+    obj = api_demo()
+    #obj.Trade_add('zet',0.01,2000)
     #obj.Trade_list('zet')
     #obj.Trade_cancel('zet','862979')
     #obj.Data_transform()
-    #obj.Trade_view('zet','864111')
+    #res=obj.Trade_view('zet','882354')
+    #print res[u'status']
     #obj.Data_transform()
-    obj.account_balance()
+    #obj.account_balance()
+    #s=obj.AllTicker()
+    #print s['zet']['last']
+    obj.Cancel_All('zet')
 
-if __name__=='__main__':
+if __name__ == '__main__':
     main()
