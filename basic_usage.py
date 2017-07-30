ae6039fec8eff61632025de20c5a2c9dd8370a47
Rocky Chen <jinweizsu@gmail.com>
2017-07-31 00:23:05 +0800

    多线程获取list

--- basic_usage.py
+++ basic_usage.py
@@ -1,4 +1,7 @@
 # -*-coding=utf-8-*-
+import multiprocessing
+import random
+
 __author__ = 'Rocky'
 '''
 http://30daydo.com
@@ -13,11 +16,13 @@ import base64
 import hashlib
 from toolkit import Toolkit
 import pandas as pd
-
+from multiprocessing import Pool,Manager
+from functools import partial
+import threading
+import Queue
 
 class api_demo():
     def __init__(self):
-
         self.host = 'http://www.jubi.com'
 
         with open('coin_list.cfg') as f:
@@ -26,7 +31,7 @@ class api_demo():
         self.d_content = json.loads(c_content)
 
         # x=zip(* d_content)
-        #print x
+        # print x
         #为什么得不到想要的结果?
 
         self.coin_list = self.d_content.keys()
@@ -95,7 +100,7 @@ class api_demo():
         parameters['signature'] = sig
         r = requests.post(url, data=parameters)
         s = r.json()
-        #print s
+        # print s
         for k, v in s.items():
 
             #print k,v
@@ -126,11 +131,12 @@ class api_demo():
         amount_outstanding - 当前剩余数量
         '''
         url = self.host + '/api/v1/trade_list/'
+        time.sleep(random.random())
         nonce = self.get_nonce_time()
         types = 'all'
-        # since=0
+        since = 0
         parameters = {'key': self.public_key, 'nonce': str(nonce), 'type': types, 'coin': coin, 'signature': ''}
-        #print parameters
+        # print parameters
         post_data = ''
         for k, v in parameters.items():
             if not isinstance(v, str):
@@ -151,15 +157,9 @@ class api_demo():
         r = requests.post(url=url, data=parameters)
         s = r.json()
         #print s
-        #print type(s)
-        '''
-        for i in s:
-            #print i
-            pass
-        '''
         return s
 
-    def Trade_add(self, coin,  price,amount):
+    def Trade_add(self, coin, price, amount):
 
         '''
         Trade_add（下单）
@@ -183,7 +183,7 @@ class api_demo():
         nonce = self.get_nonce_time()
         types = 'buy'
         # amount=100
-        #price=0.1
+        # price=0.1
         parameters = {'key': self.public_key, 'nonce': str(nonce), 'type': types, 'coin': coin, 'signature': '',
                       'amount': amount, 'price': price}
         #print parameters
@@ -238,7 +238,7 @@ class api_demo():
         for k, v in parameters.items():
             post_data = post_data + k + '=' + v + '&'
         post_data = post_data[:-1]
-        #print post_data
+        # print post_data
         post_data = post_data.replace('&signature=', '')
         signature = hmac.new(self.md5, post_data, digestmod=hashlib.sha256).digest()
         sig = self.toHex(signature)
@@ -289,16 +289,113 @@ class api_demo():
 
         r = requests.post(url=url, data=parameters)
         s = r.json()
-        print s
+
+        #print s
+        return s
+    def Trade_view_m(self, id_num, coin):
+        '''
+        Trade_view（查询订单信息）
+        Path：/api/v1/trade_view/
+        Request类型：POST
+        参数
+            key - API key
+            signature - signature
+            nonce - nonce
+            id - 挂单ID
+            coin - 币种简称,例如btc、ltc、xas
+
+
+
+            返回JSON dictionary
+            id - 挂单ID
+            datetime - 挂单时间（格式：YYYY-mm-dd HH:ii:ss）
+            type - "buy" or "sell"
+            price - 挂单价
+            amount_original - 下单时数量
+            amount_outstanding - 当前剩余数量
+            status - 状态：new(新挂单), open(开放交易), cancelled(撤消), closed(完全成交)
+            avg_price - 成交均价
+        '''
+        url = self.host + '/api/v1/trade_view/'
+        nonce = self.get_nonce_time()
+
+        parameters = {'nonce': str(nonce), 'coin': coin, 'id': id_num, 'key': self.public_key, 'signature': ''}
+        post_data = ''
+        for k, v in parameters.items():
+            post_data = post_data + k + '=' + v + '&'
+        post_data = post_data[:-1]
+        # print post_data
+        post_data = post_data.replace('&signature=', '')
+        signature = hmac.new(self.md5, post_data, digestmod=hashlib.sha256).digest()
+        sig = self.toHex(signature)
+        parameters['signature'] = sig
+
+        r = requests.post(url=url, data=parameters)
+        s = r.json()
+        #print s
         return s
 
+    def matrix_trade_list(self, coin,l,q):
+        result=[]
+        for i in l:
+            s=self.Trade_view(coin,i)
+            if s.has_key('result'):
+                print "trying again"
+                time.sleep(5*random.random())
+                s=self.Trade_view(coin,i)
+                if s.has_key('result'):
+                    print "trying again second time"
+                    time.sleep(10*random.random())
+                    s=self.Trade_view(coin,i)
+                    if s.has_key('result'):
+                        print "trying again third time"
+                        time.sleep(10*random.random())
+                        s=self.Trade_view(coin,i)
+            result.append(s)
+        q.put(result)
 
+    def get_OpenOrder_Thread(self,coin):
+
+        js = self.Trade_list(coin)
+        df=self.Data_DF(js)
+        #速度太慢，使用多进程
+        id_list=df['id'].values
+        l_id=len(id_list)
+        thread_num=10
+        thread_list=[]
+        q=Queue.Queue()
+        each_iteration=l_id/thread_num
+        for i in range(thread_num-1):
+            print id_list[i*each_iteration:(i+1)*each_iteration]
+            t=threading.Thread(target=self.matrix_trade_list,args=(coin, id_list[i*each_iteration:(i+1)*each_iteration],q))
+            thread_list.append(t)
+
+        if l_id%thread_num!=0:
+            t=threading.Thread(target=self.matrix_trade_list,args=(coin,id_list[(i+1)*each_iteration:],q))
+            thread_list.append(t)
+
+        for i in thread_list:
+            i.start()
+            time.sleep(0.5*random.random())
+        for i in thread_list:
+            i.join()
+        x_List=[]
+        while not q.empty():
+            x= q.get()
+            x_List.extend(x)
+        #print result_list
+        df=self.Data_DF(x_List)
+        print df
+
+    def get_Recent_Order(self, coin, date='2017-05-01'):
+        df = self.Trade_list(coin)
+        return df[df.index >= date]
 
     def AllTicker(self):
-        url=self.host+'/api/v1/allticker/'
-        r=requests.get(url)
-        s=r.json()
-        #print s
+        url = self.host + '/api/v1/allticker/'
+        r = requests.get(url)
+        s = r.json()
+        # print s
         '''
         for k,v in s.items():
             print self.d_content[k]
@@ -307,35 +404,81 @@ class api_demo():
             print '*'*15
         '''
         return s
-    def Cancel_All(self,coin):
-        df=self.Data_transform(coin)
-        res=df['id'].map(lambda x:self.Trade_cancel(coin,x))
+
+    def Cancel_All(self, coin):
+        df = self.Data_transform(coin)
+        res = df['id'].map(lambda x: self.Trade_cancel(coin, x))
         print res
-        #self.Trade_cancel(coin,)
+        return res
 
-    def Data_transform(self,coin):
-        data = self.Trade_list(coin)
-        df = pd.DataFrame(data)
-
-        # df['datetime']=df['datetime'].applymap(lambda x:)
-        df['datetime'] = pd.to_datetime(df['datetime'])
-        #print df
-        df.set_index('datetime',inplace=True,drop=True)
-        print df
-        print df.info()
-        print df.dtypes
-        '''
-        for i in df['id'].values:
-            self.Trade_view('zet', i)
-        '''
-        df['status']=df['id'].map(lambda x:self.Trade_view(coin,x)['status'])
-        df=df[df[u'status']==u'open']
-        self.current_order=df
+    #获取所有开放的挂单
+    def get_OpenOrder(self, coin):
+        js = self.Trade_list(coin)
+        df=self.Data_DF(js)
+        #速度太慢，使用多进程
+        df['status'] = df['id'].map(lambda x: self.Trade_view(coin, x)['status'])
+        df = df[df[u'status'] == u'open']
+        self.current_order = df
         return self.current_order
+
+    def Trade_View_Q(self,coin,x,q):
+        s=self.Trade_view(coin,x)
+        print s
+        q.put(s)
+
+    #多进程/线程 获取所有开放的挂单
+    def get_OpenOrder_MultiCore(self, coin):
+        js = self.Trade_list(coin)
+        df=self.Data_DF(js)
+        #速度太慢，使用多进程
+        id_list=df['id'].values
+        print id_list
+
+        #df['status'] = df['id'].map(lambda x: self.Trade_view(coin, x)['status'])
+        #df = df[df[u'status'] == u'open']
+        #self.current_order = df
+        #return self.current_order
+        part=partial(self.Trade_view_m,coin='zet')
+        manager=Manager()
+        q=manager.Queue()
+        cpus = multiprocessing.cpu_count()
+        p=Pool(len(id_list))
+        '''
+        for i in id_list:
+            print i
+            p.apply_async(self.Trade_View_Q,args=(coin,i,q,))
+        '''
+        resultqq=[]
+        for i in range(len(id_list)):
+            resultqq[i]=p.map(self.Trade_view_m,id_list[i])
+
+        p.close()
+        p.join()
+        print "Finish Multi-Process"
+        print resultqq
+        '''
+        result=[]
+        while not q.empty():
+            t=q.get()
+            print t
+            result.append(t)
+
+        print result
+        #df=self.Data_DF(result)
+        #print df
+        '''
+
+    #格式转换
+    def Data_DF(self, js):
+        df = pd.DataFrame(js)
+        df['datetime'] = pd.to_datetime(df['datetime'])
+        df.set_index('datetime', inplace=True, drop=True)
+        return df
+
 
 def main():
     obj = api_demo()
-    #obj.Trade_add('zet',0.01,2000)
+    # obj.Trade_add('zet',0.01,2000)
     #obj.Trade_list('zet')
     #obj.Trade_cancel('zet','862979')
     #obj.Data_transform()
@@ -345,7 +488,11 @@ def main():
     #obj.account_balance()
     #s=obj.AllTicker()
     #print s['zet']['last']
-    obj.Cancel_All('zet')
+    #obj.Cancel_All('zet')
+    #print obj.get_Recent_Order('zet', '2017-07-28')
+    #print obj.get_OpenOrder('zet')
+    #obj.get_OpenOrder_MultiCore('zet')
+    obj.get_OpenOrder_Thread('zet')
 
 if __name__ == '__main__':
     main()
